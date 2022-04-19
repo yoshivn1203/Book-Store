3164b6d1d40f9b024ddbb554ed36c7cd4515dfee
yoshivn1203 <yoshivn1203@gmail.com>
2022-04-19 23:07:01 +0700

    Order Management finish filtering function

---
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,56 @@
+﻿var dataTable;
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("delayedPayment")) {
+                loadDataTable("delayedPayment");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+
+            }
+        }
+    }
+
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll?status=" + status
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "20%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <a href="/Admin/Product/Details?orderId=${data}"
+                    class="btn btn-warning btn-sm btn4"> <i class="bi bi-pencil-square"></i> &nbsp; Details </a>
+                     `
+                },
+                "width": "10%"
+            }
+
+        ]
+    });
+}
+
+
